6f397b1712888c7427124fdc1b3f47090466a68f
Gilbert Pellegrom <gilbert@pellegrom.me>
2020-09-30 17:05:05 +0100

    Set up PHPUnit tests

---
+++ tests/TestCase.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Saaze\Tests;
+
+use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+
+class TestCase extends PHPUnitTestCase
+{
+    //
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+require dirname(__DIR__).'/vendor/autoload.php';
+
+define('SAAZE_BASE_DIR', __DIR__ . DIRECTORY_SEPARATOR . 'data');
+define('SAAZE_CONTENT_PATH', SAAZE_BASE_DIR . DIRECTORY_SEPARATOR . 'content');
+define('SAAZE_PUBLIC_PATH', SAAZE_BASE_DIR . DIRECTORY_SEPARATOR . 'public');
+define('SAAZE_TEMPLATES_PATH', SAAZE_BASE_DIR . DIRECTORY_SEPARATOR . 'templates');
+define('SAAZE_CACHE_PATH', SAAZE_BASE_DIR . DIRECTORY_SEPARATOR . 'cache');
+define('SAAZE_ENTRIES_PER_PAGE', 10);
