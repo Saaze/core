f797297f5581eced01611454c35a05ea9a817381
Gilbert Pellegrom <gilbert@pellegrom.me>
2020-09-30 20:35:56 +0100

    Rename tests data dir

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,7 @@
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-define('SAAZE_BASE_DIR', __DIR__ . DIRECTORY_SEPARATOR . 'data');
+define('SAAZE_BASE_DIR', __DIR__ . DIRECTORY_SEPARATOR . '_data');
 define('SAAZE_CONTENT_PATH', SAAZE_BASE_DIR . DIRECTORY_SEPARATOR . 'content');
 define('SAAZE_PUBLIC_PATH', SAAZE_BASE_DIR . DIRECTORY_SEPARATOR . 'public');
 define('SAAZE_TEMPLATES_PATH', SAAZE_BASE_DIR . DIRECTORY_SEPARATOR . 'templates');
