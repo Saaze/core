5091087d8c17d4be3d5daf8ef8729798b154a9af
Gilbert Pellegrom <gilbert@pellegrom.me>
2020-10-06 17:00:23 +0100

    Add support for .env config files

--- src/Saaze.php
+++ src/Saaze.php
@@ -2,9 +2,21 @@
 
 namespace Saaze;
 
+use Dotenv\Dotenv;
 
 class Saaze
 {
+    /**
+     * @param string $saazePath
+     */
+    public function __construct($saazePath)
+    {
+        define('SAAZE_PATH', $saazePath);
+
+        $dotenv = Dotenv::createImmutable($saazePath);
+        $dotenv->load();
+    }
+
     /**
      * @return void
      */
--- src/SaazeCli.php
+++ src/SaazeCli.php
@@ -2,12 +2,24 @@
 
 namespace Saaze;
 
+use Dotenv\Dotenv;
 use Saaze\Commands\BuildCommand;
 use Saaze\Commands\ServeCommand;
 use Symfony\Component\Console\Application;
 
 class SaazeCli
 {
+    /**
+     * @param string $saazePath
+     */
+    public function __construct($saazePath)
+    {
+        define('SAAZE_PATH', $saazePath);
+
+        $dotenv = Dotenv::createImmutable($saazePath);
+        $dotenv->load();
+    }
+
     public function run()
     {
         $container = container();
